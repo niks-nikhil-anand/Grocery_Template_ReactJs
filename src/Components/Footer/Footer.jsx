a42ec0f1dcfd1702f55786d18ec5344b11aa0551
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-04-26 14:05:20 +0530

    fix someting in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -78,12 +78,12 @@ const Footer = () => {
                         <div className='sm:space-y-2 space-y-1.5 xl:text-base text-sm'>
                             {/* Phone (fake) */}
                             <a className='block text-sm sm:text-base hover:underline' href="tel:+1 (406) 555-0120">
-                                <Phone fontSize='inherit' /> +91 6201060886
+                                <Phone fontSize='inherit' /> +91 9631494191
                             </a>
 
                             {/* Email */}
                             <a className='block text-sm sm:text-base hover:underline' href="mailto:hasan.dev1@gmail.com">
-                                <Mail fontSize='inherit' /> Support@dieton.com
+                                <Mail fontSize='inherit' /> Support@2solution.com
                             </a>
 
                             {/* Address (fake)*/}
@@ -99,7 +99,7 @@ const Footer = () => {
             <div>
                 <hr className='border-gray-600' />
                 <div className='text-center flex items-center h-12'>
-                    <span className='text-xs  w-full block text-gray-300 tracking-wider'>© 2024 Developed By <a className='hover:underline' href="https://2solution.in" target="_blank" rel="noopener noreferrer"> 2Solution </a></span>
+                    <span className='text-xs  w-full block text-gray-300 tracking-wider'>© 2024 Developed By 2Solution <a className='hover:underline' href="https://2solution.in" target="_blank" rel="noopener noreferrer"> 2Solution </a></span>
                 </div>
             </div>
         </footer>
